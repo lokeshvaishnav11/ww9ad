cb0fd6355efcf41bd6216afdcc8bdf5076b0fcd4
admin <admin@admins-MacBook-Pro.local>
2026-05-27 16:42:11 +0530

    FGH

--- src/admin-app/AdminRoutes.tsx
+++ src/admin-app/AdminRoutes.tsx
@@ -100,7 +100,7 @@ const AdminRoutes = () => {
             { index: true, element: <ListClients /> },
             { path: 'dashbaord', element: <AdminDashboard /> },
             { path: 'market-analysis', element: <AdminDashboard /> },
-               { path: 'id-king', element: <AdminDashboard /> },
+               { path: 'id-king/new/one', element: <AdminDashboard /> },
             { path: 'odds/:matchId', element: <Odds /> },
             { path: 'odds/:matchId/:share', element: <Odds /> },
 
